43fce590b1d9499f9abf72bea2fd767cbfc2d94b
tofaratifolayan <82730510+tofaratifolayan@users.noreply.github.com>
2022-04-16 13:20:40 -0400

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Loader} from './loader' ;
+export {default as Navbar} from './navBar' ;
+export {default as Footer} from './footer' ;
+export {default as Services} from './services' ;
+export {default as Welcome} from './welcome' ;
+export {default as Transactions} from './transactions' ;
